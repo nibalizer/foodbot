a8fb3a788c3174697b7a4e4767533f44d6467c30
Spencer Krum <krum.spencer@gmail.com>
2011-06-01 17:53:51 -0700

    works\!

--- subscribe-mailinglist.py
+++ subscribe-mailinglist.py
@@ -1,13 +1,17 @@
 
 import urllib2
+import urllib
 
 data = {'email' : 'psu.foodbot@gmail.com', 'fullname' : 'foodbotzen'}
 
-url = 'https://www.lists.pdx.edu/lists/listinfo/advocacycluster'
+url = 'https://www.lists.pdx.edu/lists/subscribe/advocacycluster'
+data = urllib.urlencode(data)
 
-foo = urllib2.Request(url, data)
+foo = urllib2.Request(url,data)
 
-urllib2.urlopen(foo)
+f = urllib2.urlopen(foo)
+
+print f.read()
 
 
 
